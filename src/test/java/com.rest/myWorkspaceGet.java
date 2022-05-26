1e65ae7bb1a991168db4d1cac254d4af6630b59c
Álefe Cardozo Silva <alefecardozo@gmail.com>
2022-05-25 21:53:37 -0300

    Create new tests WorkspacesGet

--- src/test/java/com.rest/myWorkspaceGet.java
+++ src/test/java/com.rest/myWorkspaceGet.java
@@ -3,6 +3,8 @@ package com.rest;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.junit.Test;
 
 public class myWorkspaceGet {
@@ -13,13 +15,17 @@ public class myWorkspaceGet {
     //2)generate key in MyWorkspace: https://web.postman.co/settings/me/api-keys
     //PMAK-628ab087ec9e1a3630a2d1be-aeab88f4fcae014bcfd702cff3341a8681A
 
+    String baseUri = "https://api.postman.com";
+    String xApiKey = "X-Api-Key";
+    String apiKey = "PMAK-628ab087ec9e1a3630a2d1be-aeab88f4fcae014bcfd702cff3341a8681A";
+    String path = "/workspaces";
     @Test
     public void validate_get_status_code(){
         given().
-                baseUri("https://api.postman.com").
-                header("X-Api-Key", "PMAK-628ab087ec9e1a3630a2d1be-aeab88f4fcae014bcfd702cff3341a8681A").
+                baseUri(baseUri).
+                header(xApiKey, apiKey).
                 when().
-                get("/workspaces").
+                get(path).
                 then().
                 assertThat().
                 statusCode(200).
@@ -29,10 +35,10 @@ public class myWorkspaceGet {
     @Test
     public void validate_get_status_body(){
         given().
-                baseUri("https://api.postman.com").
-                header("X-Api-Key", "PMAK-628ab087ec9e1a3630a2d1be-aeab88f4fcae014bcfd702cff3341a8681A").
+                baseUri(baseUri).
+                header(xApiKey, apiKey).
                 when().
-                get("/workspaces").
+                get(path).
                 then().
                 assertThat().
                 statusCode(200).
@@ -42,5 +48,69 @@ public class myWorkspaceGet {
                         "workspaces[0].name",is(equalTo("My Workspace")),
                         "workspaces.size()",equalTo(1)).
                 log().all();
+    }
+
+    @Test
+    public void validate_extract_response(){
+        Response response = given().
+                baseUri(baseUri).
+                header(xApiKey, apiKey).
+                when().
+                get(path).
+                then().
+                assertThat().
+                statusCode(200).
+                extract().response();
+
+        System.out.println("Extract response: " + response.asString());
+    }
+
+    @Test
+    public void validate_extract_single_value_response(){
+        //Request 01 - Using Extract with Response
+        Response response01 = given().
+                baseUri(baseUri).
+                header(xApiKey, apiKey).
+                when().
+                get(path).
+                then().
+                assertThat().
+                statusCode(200).
+                extract().response();
+
+        System.out.println("Example 01 - Extract Response -> workspaces.name = "
+                + response01.path("workspaces.name"));
+
+        //Request 02 - Using JsonPath with Response
+        Response response02 = given().
+                baseUri(baseUri).
+                header(xApiKey, apiKey).
+                when().
+                get(path).
+                then().
+                assertThat().
+                statusCode(200).
+                extract().response();
+
+        JsonPath jsonPath = new JsonPath(response02.asString());
+
+        System.out.println("Example 02 - Extract Jsonpath with Response -> workspaces.name = "
+                + jsonPath.getString("workspaces.name"));
+
+
+        //Request 03 - Using JsonPath with Response in String
+        String response03 = given().
+                baseUri(baseUri).
+                header(xApiKey, apiKey).
+                when().
+                get(path).
+                then().
+                assertThat().
+                statusCode(200).
+                extract().response().asString();
+
+        System.out.println("Example 03 - Extract with Response in String -> workspaces.name = "
+                + jsonPath.from(response03).getString("workspaces.name"));
+
     }
 }
