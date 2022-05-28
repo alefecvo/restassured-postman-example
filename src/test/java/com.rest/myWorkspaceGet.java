84b714574f1b4153612f176eb609541392cb9cdd
Álefe Cardozo Silva <alefecardozo@gmail.com>
2022-05-28 18:58:06 -0300

    create news tests using Hamcrest

--- src/test/java/com.rest/myWorkspaceGet.java
+++ src/test/java/com.rest/myWorkspaceGet.java
@@ -5,7 +5,9 @@ import static org.hamcrest.Matchers.*;
 
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+
 import org.junit.Test;
+import org.testng.Assert;
 
 public class myWorkspaceGet {
 
@@ -15,10 +17,25 @@ public class myWorkspaceGet {
     //2)generate key in MyWorkspace: https://web.postman.co/settings/me/api-keys
     //PMAK-628ab087ec9e1a3630a2d1be-aeab88f4fcae014bcfd702cff3341a8681A
 
+    //RESPONSE
+    /*
+   {
+    "workspaces": [
+        {
+            "id": "23a1c63b-9559-4b5a-847e-29465c0575b2",
+            "name": "My Workspace",
+            "type": "personal"
+        }
+    ]
+    }
+   */
+
     String baseUri = "https://api.postman.com";
     String xApiKey = "X-Api-Key";
     String apiKey = "PMAK-628ab087ec9e1a3630a2d1be-aeab88f4fcae014bcfd702cff3341a8681A";
     String path = "/workspaces";
+
+
     @Test
     public void validate_get_status_code(){
         given().
@@ -112,5 +129,94 @@ public class myWorkspaceGet {
         System.out.println("Example 03 - Extract with Response in String -> workspaces.name = "
                 + jsonPath.from(response03).getString("workspaces.name"));
 
+    }
+
+    @Test
+    public void validate_hamcrest_assert_on_extracted_response(){
+        //Assert using assertEquals
+        String response = given().
+                baseUri(baseUri).
+                header(xApiKey, apiKey).
+                when().
+                get(path).
+                then().
+                assertThat().
+                statusCode(200).
+                extract().response().path("workspaces[0].name");
+
+        Assert.assertEquals(response,"My Workspace");
+
+        //Assert using contains()
+        given().
+                baseUri(baseUri).
+                header(xApiKey, apiKey).
+                when().
+                get(path).
+                then().
+                assertThat().
+                statusCode(200).
+                body("workspaces.name",contains("My Workspace")).
+                log().all();
+
+        //Assert using containsInAnyOrder()
+        given().
+                baseUri(baseUri).
+                header(xApiKey, apiKey).
+                when().
+                get(path).
+                then().
+                assertThat().
+                statusCode(200).
+                body("workspaces.name",containsInAnyOrder("My Workspace")).
+                log().all();
+
+        //Assert using is(not(empty())
+        given().
+                baseUri(baseUri).
+                header(xApiKey, apiKey).
+                when().
+                get(path).
+                then().
+                assertThat().
+                statusCode(200).
+                body("workspaces.name",is(not(empty()))).
+                log().all();
+
+        //Assert using is(not(emptyArray())
+        given().
+                baseUri(baseUri).
+                header(xApiKey, apiKey).
+                when().
+                get(path).
+                then().
+                assertThat().
+                statusCode(200).
+                body("workspaces.name",is(not(emptyArray()))).
+                log().all();
+
+        //Assert using hasSize()
+        given().
+                baseUri(baseUri).
+                header(xApiKey, apiKey).
+                when().
+                get(path).
+                then().
+                assertThat().
+                statusCode(200).
+                body("workspaces.name",hasSize(1)).
+                log().all();
+
+
+        //Assert using everyItem(startsWith()
+        given().
+                baseUri(baseUri).
+                header(xApiKey, apiKey).
+                when().
+                get(path).
+                then().
+                assertThat().
+                statusCode(200).
+                body("workspaces.name",everyItem(startsWith("My"))).
+                log().all();
     }
 }
